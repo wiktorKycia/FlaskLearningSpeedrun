2f880ca3a78cfab90efb1df512ab8cf72fbb6f23
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-08-02 19:32:08 +0200

    set up some sonfig variables, table schema and create all at the beginning of main

--- flaskProject/sessions.py
+++ flaskProject/sessions.py
@@ -1,9 +1,23 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
-import sqlalchemy
+from flask import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = "hello"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(days=1)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -53,4 +67,5 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
