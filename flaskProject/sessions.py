43c7a5a33a82d5405412293174e4945a9cc6ae08
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-08-02 19:06:03 +0200

    install and import sqlalchemy

--- flaskProject/sessions.py
+++ flaskProject/sessions.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+import sqlalchemy
 app = Flask(__name__)
 app.secret_key = "hello"
 app.permanent_session_lifetime = timedelta(days=1)
